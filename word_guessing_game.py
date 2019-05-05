165cb012bfe4a092f8234725ccd78cddaca8d6f0
Ethan Miller <ethdmill@gmail.com>
2019-05-05 05:30:31 -0400

    added a lot more stuff, finished core functionality, added more errors, etc.

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -10,104 +10,44 @@
 # [x] word is shown to user as ------- with correctly guessed letters displayed
 # [x] be sure to send every user input to lowercase just in case of auto-caps
 # [x] if initial input isn't Y or N, throw an error
-# [ ] if user guess is correct, display answer_dashes but replace dashes with correct letters guessed
-# [ ] if guess input is a number, throw an error
+# [x] if user guess is correct, display answer_dashes but replace dashes with correct letters guessed
+# [x] if guess input is a number, throw an error
 # [x] display congratulations message on whole word success, better luck next time on failure
 # [x] ask if user would like to play again; if yes, loop, if no, break loop
-# [ ] add more words to word_bank
-# [ ] test functionality, check for line spacing
+# [x] test functionality, check for line spacing
 # [ ] comment all the things
+# [ ] add more words to word_bank
 
 
 import random
 
 
-# Start-up message
-print("Hello! Let's play a word game!\n")
-
+# Initial variable declarations to keep loops in line
 user_continue = True
+win = False
+main_loop = True
 
-while user_continue == True:
 
-    # Initial variable declarations
-    word_bank = ["vociferous"]
-    answer = random.choice(word_bank)
-    answer_dashes = "-" * len(answer)
-    attempts = 0
-
-    # Initial input to start game
+# Function that runs at end of game, accessed by changing win conditions
+def end_of_game():
     try:
-        start = input("Please press Y to begin or N to quit.\n").lower()
 
-        # Conditional to begin game
-        if start == "y":
+        # This variable needs to be global to affect main user_continue loop
+        global user_continue
 
-            # Answer length check to determine user's number of guess attempts
-            if len(answer) <= 5:
-                attempts = 5
-                print("You have {} attempts left.".format(attempts))
-            elif 5 < len(answer) <= 10:
-                attempts = 10
-                print("You have {} attempts left.".format(attempts))
-            elif 10 < len(answer) <= 15:
-                attempts = 15
-                print("You have {} attempts left.".format(attempts))
-            else:
-                attempts = 20
-                print("You have {} attempts left.".format(attempts))
+        keep_going = input("Would you like to keep going? Y/N\n").lower()
 
-            # Nested loop for guessing letters, based on number of remaining attempts
-            win = False
-            win_condition = False
-            temp_answer = answer_dashes
-            while attempts > 0 and win == False:
+        # If they want to continue, loop restarts
+        if keep_going == "y":
+            user_continue = True
 
-                try:
-                    guess = input("Guess a letter, or guess the whole word!\n").lower()
+        # If they don't want to continue, loop breaks, program ends
+        elif keep_going == "n":
+            print("Thanks for playing!")
+            user_continue = False
+            main_loop = False
 
-                    if guess.isalpha():
-
-                        if guess in answer and guess != answer:
-
-                            # code to go here:
-
-                            # guess is checked against answer
-                            # temp_answer turns into some combination of answer_dashes and answer_dashes
-
-                            if temp_answer == answer:
-                                win_condition = True
-                                break
-
-                            else:
-                                print("So far, you have {}.\n".format(temp_answer))
-                                print("Nice work, keep going!")
-
-
-                        elif guess == answer:
-                            win_condition = True
-                            break
-
-                        else:
-                            attempts -= 1
-                            if attempts > 1:
-                                print("Oops! You have {} attempts left.\n".format(attempts))
-                            elif attempts == 1:
-                                print("Careful, you only have {} attempt left!\n".format(attempts))
-                            else:
-                                print("Oh no, you're out of attempts! Please try again!\n")
-
-                    else: raise ValueError("Please enter letters only!\n")
-
-                except ValueError as err:
-                    print("{}".format(err))
-
-
-        # Exit message if user selected N at very beginning
-        elif start.lower() == "n":
-            print("Come back soon!")
-            break
-
-        # Error in case user enters some other random letter/number/whatever
+        # If they enter some inane value, error throws
         else:
             raise ValueError("Oops, please enter either Y or N!\n")
 
@@ -115,21 +55,142 @@ while user_continue == True:
         print("{}".format(err))
 
 
-    # If win_condition is met, function at top of program is called, prompting replay
-    if win_condition == True:
-        
-        print("Ding ding ding! The answer was {}!\nCongratulations, you win!\n\n".format(answer))
 
-        try:
-            keep_going = input("Would you like to keep going? Y/N\n").lower()
-            if keep_going == "y":
-                win = True
-            elif keep_going == "n":
-                print("Thanks for playing!")
-                user_continue = False
-                break
-            else:
-                raise ValueError("Oops, please enter either Y or N!\n")
+# Start-up message
+print("Hello! Let's play a word game!\n")
 
-        except ValueError as err:
-            print("{}".format(err))
+
+while main_loop == True:
+
+    # Initial input to start the game
+    start = input("Please press Y to begin or N to quit.\n").lower()
+
+    try:
+        if start.isalpha() and (start == "y" or start == "n"):
+
+            while user_continue == True:
+
+                # Initial variable declarations
+                word_bank = ["these", "are", "test", "words"]
+                answer = random.choice(word_bank)
+                answer_dashes = "-" * len(answer)
+                attempts = 0
+
+
+                # Conditional to begin game
+                if start == "y":
+
+                    # Answer length check to determine user's number of guess attempts
+                    if len(answer) <= 5:
+                        attempts = 5
+                        print("You have {} attempts left.\n".format(attempts))
+                    elif 5 < len(answer) <= 10:
+                        attempts = 10
+                        print("You have {} attempts left.\n".format(attempts))
+                    elif 10 < len(answer) <= 15:
+                        attempts = 15
+                        print("You have {} attempts left.\n".format(attempts))
+                    else:
+                        attempts = 20
+                        print("You have {} attempts left.\n".format(attempts))
+
+                    # Nested loop for guessing letters, based on number of remaining attempts
+                    print(answer_dashes)
+                    win = False
+                    temp_answer = answer_dashes
+                    while attempts > 0 and win == False:
+
+                        try:
+                            guess = input("Guess a letter, or guess the whole word!\n").lower()
+
+                            # Ensures guess is comprised of letters
+                            if guess.isalpha():
+
+                                try:
+                                    # Ensures guess is either a single letter or the length of the answer
+                                    if len(guess) == 1 or len(guess) == len(answer):
+
+                                        temp_answer = list(answer_dashes)
+
+                                        # If their guess is part of answer but isn't the whole answer, this runs
+                                        if guess in answer and guess != answer:
+
+                                            guess_position = -1
+                                            i = 0
+                                            while i < len(answer):
+
+                                                temp_answer = list(temp_answer)
+
+                                                # This stores the index of every guess in answer as guess_position once per iteration without repeating indeces
+                                                guess_position = answer.find(guess, guess_position + 1)
+
+                                                if guess_position != -1 and guess == answer[guess_position]:
+                                                    temp_answer[guess_position] = guess
+
+                                                if guess_position == -1:
+                                                    break
+
+                                            temp_answer = "".join(temp_answer)
+
+                                            # Once the culmination of their guesses matches answer, they win
+                                            if temp_answer == answer:
+                                                win = True
+                                                break
+
+                                            else:
+                                                print("\nSo far, you have {}.".format(temp_answer))
+                                                print("Nice work, keep going!\n")
+
+                                        # If their guess is the whole word, immediate win
+                                        elif guess == answer:
+                                            win = True
+                                            break
+
+                                        # If their guess doesn't appear in answer, they lose an attempt
+                                        else:
+                                            attempts -= 1
+                                            if attempts > 1:
+                                                print("Oops! You have {} attempts left.\n".format(attempts))
+                                                temp_answer = "".join(temp_answer)
+                                                print(temp_answer)
+                                            elif attempts == 1:
+                                                print("Careful, you only have {} attempt left!\n".format(attempts))
+                                                temp_answer = "".join(temp_answer)
+                                                print(temp_answer)
+                                            else:
+                                                print("Oh no, you're out of attempts!\nThe answer was \"{}\". Please try again!\n".format(answer))
+                                                end_of_game()
+
+                                    else:
+                                        raise ValueError("Oops, you can only enter a single letter or the exact length of the word!\nThe length of the word is represented by the dashes.\n")
+                                        temp_answer = "".join(temp_answer)
+                                        print(temp_answer)
+
+                                except ValueError as err:
+                                    print("{}".format(err))
+
+
+                            else:
+                                raise ValueError("Please enter letters only!\n" + "\n" + temp_answer)
+
+                        except ValueError as err:
+                            print("{}".format(err))
+
+
+                # Exit message if user selected N at very beginning
+                elif start == "n":
+                    print("Come back soon!")
+                    main_loop = False
+                    break
+
+
+                # If win_condition is met, function at top of program is called, prompting replay
+                if win == True:
+                    print("Ding ding ding! The answer was \"{}\"!\nCongratulations, you win!\n\n".format(answer))
+                    end_of_game()
+
+        else:
+            raise ValueError("Oops, please enter either Y or N!\n")
+
+    except ValueError as err:
+        print("{}".format(err))
